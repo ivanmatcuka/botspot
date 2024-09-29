f9fd7ffa9790723b21fcddabe54416c8d76bc6a8
Ivan Matcuka <ivanmatcuka@protonmail.com>
2024-09-29 13:21:57 +0200

    refactor: rudiments removed

--- src/app/components/ScrollableVideo/ScrollableVideo.tsx
+++ src/app/components/ScrollableVideo/ScrollableVideo.tsx
@@ -1,5 +1,4 @@
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
-import useDetectScroll from '@smakss/react-scroll-direction';
+import { FC, useEffect, useRef, useState } from 'react';
 
 type ScrollableVideoProps = {
   videoSrc: string;
@@ -10,43 +9,7 @@ export const ScrollableVideo: FC<ScrollableVideoProps> = ({
   autoplay,
 }) => {
   const [video, setVideo] = useState<HTMLVideoElement | null>(null);
-  const [videoProgress, setVideoProgress] = useState(0);
-  const [isInViewport, setIsInViewport] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
-  const { scrollDir } = useDetectScroll({
-    target: window,
-  });
-
-  const checkInViewport = useCallback(() => {
-    if (!video) return;
-
-    const rect = video.getBoundingClientRect();
-    const viewportHeight = window.innerHeight;
-    const elementHeight = rect.height;
-
-    const top = rect.top;
-    const bottom = Math.min(rect.bottom, viewportHeight);
-
-    // Check if the video is within the viewport
-    if (top < 0 || bottom > viewportHeight) false;
-
-    // Calculate the center point of the viewport
-    const viewportCenter = viewportHeight / 2;
-
-    // Calculate the center point of the video element
-    const elementCenter = top + elementHeight / 2;
-
-    // Check if the video is within a certain threshold of the viewport center
-    const threshold = 300; // adjust this value as needed
-    return Math.abs(elementCenter - viewportCenter) < threshold;
-  }, [video]);
-
-  useEffect(() => {
-    if (autoplay || !video) return;
-    const newTime = videoProgress * video.duration;
-
-    video.currentTime = isNaN(newTime) ? 0 : newTime;
-  }, [autoplay, video, videoProgress]);
 
   useEffect(() => {
     const onScroll: EventListener = () => {
@@ -59,15 +22,15 @@ export const ScrollableVideo: FC<ScrollableVideoProps> = ({
         1,
       );
 
-      setVideoProgress(progress);
-      setIsInViewport(!!checkInViewport());
+      if (autoplay || !video) return;
+      const newTime = progress * video.duration;
+
+      video.currentTime = isNaN(newTime) ? 0 : newTime;
     };
     window.addEventListener('scroll', onScroll);
 
     return () => window.removeEventListener('scroll', onScroll);
   }, [video]);
-
-  useEffect(() => {}, []);
 
   return (
     <div className="h-full relative" ref={containerRef}>
