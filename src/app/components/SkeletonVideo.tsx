bb16b1161e5164dc5cde5f6fae06c76df9bdc3a1
Ivan Matcuka <ivanmatcuka@gmail.com>
2024-11-22 16:22:05 +0100

    chore: add videoRef and Skeleton component; update onLoadedData logic

--- src/app/components/SkeletonVideo.tsx
+++ src/app/components/SkeletonVideo.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { FC, MediaHTMLAttributes, useState } from 'react';
+import { Skeleton } from '@mui/material';
+import { FC, MediaHTMLAttributes, useRef, useState } from 'react';
 
 type SkeletonVideoProps = {
   videoSrc: string;
@@ -11,22 +12,25 @@ export const SkeletonVideo: FC<SkeletonVideoProps> = ({
   ...props
 }: SkeletonVideoProps) => {
   const [isLoaded, setIsLoaded] = useState(false);
-
-  console.log(isLoaded);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   return (
     <div className={`relative ${className}`}>
-      {/* <Skeleton
+      <video
+        className={`w-full h-full relative ${className}`}
+        ref={videoRef}
+        src={videoSrc}
+        controls
+        onLoadedData={() => {
+          if (videoRef.current?.readyState !== 4) return;
+          setIsLoaded(true);
+        }}
+        {...props}
+      />
+      <Skeleton
         className={`rounded-xl absolute inset-0 ${isLoaded && '!hidden'} z-10`}
         height={'100%'}
         variant="rectangular"
-      /> */}
-      <video
-        className={`w-full h-full relative ${className}`}
-        src={videoSrc}
-        controls
-        onCanPlayThrough={() => setIsLoaded(true)}
-        {...props}
       />
     </div>
   );
