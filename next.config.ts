4f0500c8eccc3b5332f1a470058bb3f4a7c4487b
Ivan Matcuka <ivanmatcuka@gmail.com>
2025-03-31 16:50:54 +0200

    refactor: remove old environment files and migrate configuration to TypeScript

---
+++ next.config.ts
@@ -0,0 +1,60 @@
+import { getRedirects } from './src/service';
+
+import { NextConfig } from 'next';
+
+type JsonData = {
+  url: string;
+  action_code: number;
+  match_url: string;
+  action_data: {
+    url?: string;
+    url_from?: string;
+    server?: string;
+  };
+}[];
+
+const adaptRedirectsForNextJs = (jsonData: JsonData) =>
+  jsonData.map((redirect) => {
+    const { url, action_data, action_code, match_url } = redirect;
+    const { url: action_url, url_from, server } = action_data;
+
+    const source = match_url.replace(/\/$/, '');
+    const destination = action_url || url_from || server || url;
+
+    return {
+      source,
+      destination,
+      permanent: action_code === 301,
+    };
+  });
+
+const nextConfig: NextConfig = {
+  sassOptions: {
+    silenceDeprecations: ['color-functions', 'mixed-decls', 'legacy-js-api'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'botspot.live-website.com',
+        port: '',
+      },
+    ],
+  },
+  experimental: {
+    inlineCss: true,
+  },
+  async redirects() {
+    const jsonData = await getRedirects();
+    const nextJsRedirects = adaptRedirectsForNextJs(jsonData.data);
+
+    return nextJsRedirects;
+  },
+};
+
+export default nextConfig;
