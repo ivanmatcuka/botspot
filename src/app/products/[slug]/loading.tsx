bb9c4f2a2a376da30a4e476b3998e53978f28d88
Ivan Matcuka <ivanmatcuka@gmail.com>
2025-04-02 23:26:02 +0200

    refactor: update Loading component to use flexible height for improved responsiveness

--- src/app/products/[slug]/loading.tsx
+++ src/app/products/[slug]/loading.tsx
@@ -1,5 +1,12 @@
 import { Skeleton } from '@mui/material';
 
 export default function Loading() {
-  return <Skeleton height="100vh" variant="rectangular" width="100%" />;
+  return (
+    <Skeleton
+      height="auto"
+      sx={{ flex: 1 }}
+      variant="rectangular"
+      width="100%"
+    />
+  );
 }
